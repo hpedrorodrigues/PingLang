4193359ec4590d5d9e58c38c4c19e0450722c258
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2017-05-19 17:21:58 -0300

    Adding more tests to parser

--- test/parser_test.js
+++ test/parser_test.js
@@ -42,5 +42,48 @@ describe('Parser', function () {
                 ]
             });
         });
+
+        it('should return an ast when a simple line with log to stdout with only text is given', function () {
+            let tokens = [
+                {type: 'keyword', label: 'print', value: 'show'},
+                {type: 'double_quotes', value: '"'},
+                {type: 'word', value: 'Test'},
+                {type: 'word', value: 'Test'},
+                {type: 'double_quotes', value: '"'}
+            ];
+
+            let ast = new Parser(tokens);
+
+            Chai.expect(ast).to.deep.equal({
+                type: 'ping',
+                body: [
+                    {type: 'CallExpression', label: 'print', arguments: 'Test Test'}
+                ]
+            });
+        });
+
+        it('should return an ast when logs and comments are given in multiple lines', function () {
+            let tokens = [
+                {type: 'keyword', label: 'print', value: 'show'},
+                {type: 'double_quotes', value: '"'},
+                {type: 'word', value: 'Test'},
+                {type: 'number', value: '1'},
+                {type: 'double_quotes', value: '"'},
+                {type: 'new_line'},
+                {type: 'comment'},
+                {type: 'word', value: 'Test'},
+                {type: 'number', value: '2'}
+            ];
+
+            let ast = new Parser(tokens);
+
+            Chai.expect(ast).to.deep.equal({
+                type: 'ping',
+                body: [
+                    {type: 'CallExpression', label: 'print', arguments: 'Test 1'},
+                    {type: 'CommentExpression', label: '#', arguments: 'Test 2 '}
+                ]
+            });
+        });
     });
 });
