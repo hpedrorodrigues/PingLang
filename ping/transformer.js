61ada198e0612f86867f642fa2fb588ae37f9ad7
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2017-05-18 23:53:02 -0300

    Starting transformer

--- ping/transformer.js
+++ ping/transformer.js
@@ -3,7 +3,29 @@ export default function Transformer(ast) {
     this.ast = ast;
 
     this.generateJSAST = function () {
-        return this.ast;
+
+        let AST = {
+            type: 'javascript',
+            body: []
+        };
+
+        while (this.ast.body.length) {
+            let node = this.ast.body.shift();
+
+            if (node.type === 'CallExpression') {
+
+                if (node.name === 'print') {
+                    let newNode = {
+                        object: 'console',
+                        arguments: node.arguments.join(' ')
+                    };
+
+                    AST.push(newNode);
+                }
+            }
+        }
+
+        return AST;
     };
 
     return this.generateJSAST();
